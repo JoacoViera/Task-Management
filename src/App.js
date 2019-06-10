2b913734877b4c6202dada341a9a67839690c2c3
joacoviera <joaquin.viera@outook.com>
2019-06-10 19:13:32 -0300

    Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Navigation from './components/Navigation.js';
+
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
   
-  return (
-    <div className="App">
-
-      <nav class="navbar navar-dark">
-
-      </nav>
-      
-      
-      <img src={logo} className="App-logo" alt="logo" />
-      
-    </div>
-  );
+        <Navigation />
+        <Navigation />
+        <Navigation />
+                
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render(){
+    return (
+      <div className="Navigation">
+  
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="https://getbootstrap.com/docs/4.3/components/navbar/">Navegacion</a>
+        </nav>
+        
+        
+      </div>
+    );
+  }
+}
+
+export default Navigation;
