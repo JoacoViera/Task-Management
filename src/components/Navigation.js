4e4ddc77a4b5bccf83ff99ccf74a6098049a80d2
Joaquin <joaquin.viera@outlook.com>
2019-08-28 17:43:17 -0300

    hola

---
+++ src/components/Navigation.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render(){
+    return (
+      <div className="Navigation">
+  
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="#" className="text-white">
+
+            {/* Props para enviar atributos a traves del component */}
+            {/* Dentro de estas llaves puedo meter js como un champion */}
+            { this.props.titulo}
+          </a>
+        </nav>
+             
+      </div>
+    );
+  }
+}
+
+export default Navigation;
