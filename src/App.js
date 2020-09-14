5b0966bebcd3412b8e9731c18b5de91fc5908280
JoacoViera <joaquin.viera@outlook.com>
2020-09-14 03:36:55 -0300

    now with hooks and functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,89 +6,73 @@ import TodoForm from "./components/TodoForm";
 import { todos } from "./todos.json";
 
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos,
-    };
-    this.removeTodo = this.removeTodo.bind(this);
-    this.handleAddTodo = this.handleAddTodo.bind(this);
+const App = (props) => {
+
+  const [tasks, setTasks] = useState(todos);
+
+  const handleAddTodo = (todo) => {
+    setTasks([...tasks, todo]);
   }
 
-  handleAddTodo(todo) {
-    this.setState({
-      todos: [...this.state.todos, todo],
-    });
-  }
-
-  removeTodo(index) {
-    if (window.confirm("Are you sure you want to delete it?")) {
-      this.setState({
-        // Filter devuelve un nuevo arreglo
-        todos: this.state.todos.filter((e, i) => {
-          return i !== index;
-        }),
-      });
-    }
-  }
-
-  render() {
-    const todos = this.state.todos.map((todo, i) => {
-      return (
-        <div className="col-md-4" key={i}>
-          <div className="card mt-4">
-            <div className="card-header bg-info">
-              <h3>{todo.title}</h3>
-              <span className="badge badge-pill badge-danger" ml-2>
-                {todo.priority}
-              </span>
-            </div>
-            <div className="card-header bg-light">
-              <h3>{todo.description}</h3>
-              <p>
-                <mark>{todo.responsible}</mark>
-              </p>
-            </div>
-            <div className="card-footer">
-              <button
-                className="btn btn-danger"
-                onClick={this.removeTodo.bind(this, i)}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
-      );
-    });
-
+  const taskComponent = tasks.map((todo, i) => {
     return (
-      <div className="App">
-        <nav className="navbar navbar-dark bg-dark">
-          <a href="https://www.google.com" className="text-white">
-            Tasks
-            <span className="badge badge-pill badge-light ml-2">
-              {this.state.todos.length}
+      <div className="col-md-4" key={i}>
+        <div className="card mt-4">
+          <div className="card-header bg-info">
+            <h3>{todo.title}</h3>
+            <span className="badge badge-pill badge-danger" ml-2>
+              {todo.priority}
             </span>
-          </a>
-        </nav>
-
-        <div className="container">
-          <div className="row mt-4">
-            <div className="col-md-3">
-              <img src={logo} className="App-logo" alt="logo" />
-              <TodoForm onAddTodo={this.handleAddTodo} />
-            </div>
-            <div className="col-md-9">
-              <div className="row">{todos}</div>
-            </div>
+          </div>
+          <div className="card-header bg-light">
+            <h3>{todo.description}</h3>
+            <p>
+              <mark>{todo.responsible}</mark>
+            </p>
+          </div>
+          <div className="card-footer">
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                if (window.confirm("Are you sure you want to delete it?")) {
+                  let t = tasks.filter((e, h) => { return i !== h; });
+                  setTasks(t);
+                }
+              }}
+            >
+              Delete
+              </button>
           </div>
         </div>
-
-      </div>
+      </div >
     );
-  }
+  });
+
+  return (
+    <div className="App">
+      <nav className="navbar navbar-dark bg-dark">
+        <a href="https://www.google.com" className="text-white">
+          Tasks
+            <span className="badge badge-pill badge-light ml-2">
+            {todos.length}
+          </span>
+        </a>
+      </nav>
+
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-md-3">
+            <img src={logo} className="App-logo" alt="logo" />
+            <TodoForm onAddTodo={handleAddTodo} />
+          </div>
+          <div className="col-md-9">
+            <div className="row">{taskComponent}</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default App;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,82 +1,95 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class TodoForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "",
-      responsible: "",
-      description: "",
-      priority: "low",
-    };
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const TodoForm = (props) => {
+  const { onAddTodo } = props;
+  const [title, setTitle] = useState('');
+  const [responsible, setResponsible] = useState('');
+  const [description, setDescription] = useState('');
+  const [priority, setPriority] = useState('low');
 
-  handleInput(e) {
-    const { value, name } = e.target;
-    this.setState({
-      [name]: value,
-    });
-    console.log(this.state);
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.onAddTodo(this.state);
-    console.log(this.state);
-    console.log("sendig the data...");
+    if (!title) {
+      alert("PLEASE ADD A TITTLE");
+    } else if (!responsible) {
+      alert("PLEASE ADD A RESPONSIBLE");
+    } else if (!description) {
+      alert("PLEASE ADD A DESCRIPTION");
+    } else {
+      onAddTodo(
+        {
+          "title": title,
+          "responsible": responsible,
+          "description": description,
+          "priority": priority
+        }
+      );
+    }
   }
 
-  render() {
-    return (
-      <div className="card">
-        <form className="card-body" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <input
-              type="text"
-              name="title"
-              onChange={this.handleInput}
-              className="form-control"
-              placeholder="Title"
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              name="responsible"
-              onChange={this.handleInput}
-              className="form-control"
-              placeholder="Responsible"
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              name="description"
-              onChange={this.handleInput}
-              className="form-control"
-              placeholder="Description"
-            />
-          </div>
-          <div className="form-group">
-            <select
-              name="priority"
-              className="form-control"
-              onChange={this.handleInput}
-            >
-              <option>low</option>
-              <option>medium</option>
-              <option>high</option>
-            </select>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Save
-          </button>
-        </form>
-      </div>
-    );
+
+  const handleInput = (e) => {
+    const { value, name } = e.target;
+    if (name === 'title') {
+      setTitle(value)
+    } else if (name === 'responsible') {
+      setResponsible(value);
+    } else if (name === 'description') {
+      setDescription(value);
+    } else if (name === 'priority') {
+      setPriority(value)
+    } else {
+      console.log('error');
+    }
   }
+
+  return (
+    <div className="card">
+      <form className="card-body" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="title"
+            onChange={handleInput}
+            className="form-control"
+            placeholder="Title"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="responsible"
+            onChange={handleInput}
+            className="form-control"
+            placeholder="Responsible"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="description"
+            onChange={handleInput}
+            className="form-control"
+            placeholder="Description"
+          />
+        </div>
+        <div className="form-group">
+          <select
+            name="priority"
+            className="form-control"
+            onChange={handleInput}
+          >
+            <option>low</option>
+            <option>medium</option>
+            <option>high</option>
+          </select>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Save
+          </button>
+      </form>
+    </div>
+  );
 }
 
 export default TodoForm;
