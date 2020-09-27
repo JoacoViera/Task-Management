a5eb115fffb68ab57d6345bffa9b65d54dbeec16
JoacoViera <joaquin.viera@outlook.com>
2020-09-26 23:17:48 -0300

    Updates

---
+++ precache-manifest.266b3ceb7b0afc46db29e60c9c1a1f1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db2bab031bde33eafb466f81821007d",
+    "url": "/Task-Management/index.html"
+  },
+  {
+    "revision": "8fa8a7359e24a0a29289",
+    "url": "/Task-Management/static/css/main.29116bd6.chunk.css"
+  },
+  {
+    "revision": "b78b10d01b8b7c601924",
+    "url": "/Task-Management/static/js/2.adf8e7d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-Management/static/js/2.adf8e7d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa8a7359e24a0a29289",
+    "url": "/Task-Management/static/js/main.aaa75944.chunk.js"
+  },
+  {
+    "revision": "6b7935f464380e74096c",
+    "url": "/Task-Management/static/js/runtime-main.3ef2b79a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Task-Management/static/media/logo.ee7cd8ed.svg"
+  }
+]);
